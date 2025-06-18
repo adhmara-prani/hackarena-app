52a93cf53e96671f108ccf4768bfd97f2d3013e9
Naman1031 <narora05@outlook.com>
2025-06-18 17:39:15 +0530

    react router dom added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import LandingPage from './pages/LandingPage'
+import { useState } from "react";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <LandingPage/>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
