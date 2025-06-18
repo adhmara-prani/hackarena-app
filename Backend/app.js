1139e8c922abc8ac9c4d407f7719210927fb90d8
Naman1031 <narora05@outlook.com>
2025-06-18 18:33:05 +0530

    User model done

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import dataRouter from './routes/data.routes.js'
+import userRouter from './routes/user.routes.js'
+import cookieParser from 'cookie-parser';
 const app = express();
+
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
@@ -10,5 +14,14 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+app.use('/api/data',dataRouter)
+
+//user routes
+app.use('/api/users',userRouter)
+
+
 
 export { app }
+
