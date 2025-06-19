03421f3672fa4a207a3d137def91844bb46acd94
Naman1031 <narora05@outlook.com>
2025-06-19 16:16:55 +0530

    backend routes done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { login, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup } from '../controllers/auth.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = express.Router()
 
 router.route('/register').post(signup);
 
 router.route('/login').post(login);
+
+router.route('/logout').post(verifyJWT,logout)
+
+
 
 export default router
